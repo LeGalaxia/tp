ad893e4a3b258c9d3032135eea697780cbb17c43
LeGalaxia <126244195+LeGalaxia@users.noreply.github.com>
2024-10-21 09:28:40 +1100

    import des fichier

---
+++ compteur click/compteur.js
@@ -0,0 +1,8 @@
+var nombreClics = 0;
+
+function comptage() {
+  nombreClics++;
+  document.getElementById("nombreClics").textContent = nombreClics;
+}
+
+document.getElementById("boutonClic").addEventListener("click", comptage);
